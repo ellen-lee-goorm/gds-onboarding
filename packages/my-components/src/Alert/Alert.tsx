65749024b1e84b9c1aae13f6ef4f24c40daaf403
ellen-lee-goorm <ellen.lee@goorm.io>
2024-09-21 17:52:19 +0900

    style: 세미콜론 및 오탈자 수정

--- packages/my-components/src/Alert/Alert.tsx
+++ packages/my-components/src/Alert/Alert.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { AlertProps } from "./Alert.types";
+import { AlertProps } from './Alert.types';
 import styles from './Alert.module.scss';
 
-export default function Alert({status='primary', children}: AlertProps){
-    return (
-        <div className={`container ${styles[`alert${status}`]}`} role='alert'>{children}</div>
-    )
+export default function Alert({ status = 'primary', children }: AlertProps) {
+  return (
+    <div
+      className={`${styles.container} ${styles[`alert-${status}`]}`}
+      role="alert"
+    >
+      {children}
+    </div>
+  );
 }
