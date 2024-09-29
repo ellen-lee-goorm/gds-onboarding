61bb85291114edeba51c740c5470432a87e4f933
ellen-lee-goorm <ellen.lee@goorm.io>
2024-09-30 01:44:40 +0900

    build: rollup 관련 의존성 설치 및 설정

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,85 @@
+import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import postcss from 'rollup-plugin-postcss';
-import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
+import autoprefixer from 'autoprefixer';
 import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import fs from 'fs';
+import { readPackageUpSync } from 'read-package-up';
+import path from 'path';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/cjs/index.js',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/esm/index.js',
-      format: 'es',
-    },
-  ],
-  plugins: [
-    json(),
-    postcss({
-      extension: ['.css', '.scss'],
-      use: [['sass']],
-      plugins: [autoprefixer()],
-      minimize: true,
-    }),
-    typescript(),
-    nodeResolve(),
-    commonjs(),
-    babel({
-      babelHelpers: 'runtime',
-      extensions: ['.ts', '.tsx', '.js', '.jsx'],
-      presets: [['react-app', { flow: false, typescript: true }]],
-      // exclude
-    }),
-    terser(),
-    peerDepsExternal(),
-  ],
-};
+const { packageJson: pkg } = readPackageUpSync({
+  cwd: fs.realpathSync(process.cwd()),
+});
+
+const pkgDependencies = Object.keys(pkg.dependencies || {});
+const pkgPeerDependencies = Object.keys(pkg.peerDependencies || {});
+const pkgOptionalDependencies = Object.keys(pkg.optionalDependencies || {});
+
+const input = 'index.ts';
+
+export default [
+  buildJS(input, pkg.main, 'cjs'),
+  buildJS(input, 'dist/esm', 'es'),
+];
+
+function buildJS(input, output, format) {
+  const defaultOutputConfig = {
+    format,
+    sourcemap: true,
+  };
+
+  const esOutputConfig = {
+    ...defaultOutputConfig,
+    preserveModules: true,
+    preserveModulesRoot: path.dirname(input),
+    exports: 'auto',
+    dir: output,
+  };
+
+  const cjsOutputConfig = {
+    ...defaultOutputConfig,
+    preserveModules: false,
+    exports: 'named',
+    file: output,
+  };
+
+  const config = {
+    input,
+    output: format === 'es' ? esOutputConfig : cjsOutputConfig,
+    external: pkgDependencies
+      .concat(pkgPeerDependencies)
+      .concat(pkgOptionalDependencies),
+
+    plugins: [
+      postcss({
+        extract: true,
+        autoModules: true,
+        extension: ['.css', '.scss'],
+        use: [['sass']],
+        plugins: [autoprefixer()],
+        minimize: true,
+      }),
+      json(),
+      nodeResolve({
+        extensions: ['.js', '.ts', '.tsx', '.css', '.scss'],
+      }),
+      commonjs({
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      }),
+      babel({
+        babelHelpers: 'bundled',
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        presets: [
+          '@babel/preset-env',
+          '@babel/preset-react',
+          '@babel/preset-typescript',
+        ],
+      }),
+      terser(),
+    ],
+  };
+
+  return config;
+}
